50e99b42e52599ba58c9adcdeb5ac763d47ffe89
Taelyth <jaquebeneduzzi@hotmail.com>
2021-08-12 22:36:23 -0300

    Conclusão do curso de Rest-Assured e Java

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 
 public class Pet {
     //documentação do swagger utilizado: https://petstore.swagger.io/
@@ -17,7 +19,7 @@ public class Pet {
     }
 
     // Incluir - Create - Post
-    @Test //Identifica a função/método como um teste para o TestNG
+    @Test(priority = 1) //Identifica a função/método como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("src/test/resources/db/pet1.json");
 
@@ -33,7 +35,70 @@ public class Pet {
                 .post(uri)
         .then()
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("Snoopy"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("SemanaAPI"))
+        ;
     }
+
+    String petId = "458712";
+
+    @Test(priority = 2)
+    public void consultarPet()
+    {
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Snoopy"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("SemanaAPI"))
+        .extract()
+                .path("status")
+        ;
+        System.out.println("o token eh: " + token);
+    }
+
+    @Test
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("src/test/resources/db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Snoopy"))
+                .body("status", is("sold"))
+        ;
+    }
+
+    @Test
+    public void excluirPet()
+    {
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("message", is(petId))
+                ;
+    }
+
 
 }
